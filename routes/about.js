3d36b856d2c296cf7e2f2f03b9720f1620623ddb
shlomige <shlomi@geva4u.com>
2025-06-08 00:39:43 +0300

    add about endpoint

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('about page');
+    res.send([{first_name:'Elisha', last_name:'Lapid'},{first_name:'Shlomo',last_name:'geva'}]);
 });
 
 module.exports = router;
