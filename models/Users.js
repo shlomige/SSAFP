71305077cdea02c3d0d0657250d738a320231648
shlomige <shlomi@geva4u.com>
2025-06-08 00:26:30 +0300

    add add route and fix expense schema

--- models/Users.js
+++ models/Users.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true }
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    birthday: { type: Date, required: true },
+    marital_status: { type: String, required: true }
 });
 
 module.exports = mongoose.model('User', userSchema);
--- routes/add.js
+++ routes/add.js
@@ -1,19 +1,33 @@
 var express = require('express');
 var router = express.Router();
+const Expense = require('../models/Expense');
 
 /* GET users listing. */
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
     try {
         const description = req.body.description;
         const category = req.body.category;
-        const userid = req.body.id;
+        const userid = req.body.userid;
         const sum = req.body.sum;
-        res.send({description, category, userid, sum});
+        if (!description || !category || !sum || !userid) {
+          return res.status(400).send({"missing required parameters": true});
+
+        }else {
+            const expense = new Expense({userid,description,category,sum});
+             await expense.save()
+            //console.log(description, category, sum,userid);
+            return res.status(201).send(expense);
+        }
     }
     catch (error) {
-        res.status(500).send({error: error.message});
+        console.error("Error adding expense:", error);
+        if (error.name === 'ValidationError') {
+            return res.status(400).send({ message: "Validation failed", errors: error.errors });
+        }
+
+        // For any other unexpected errors
+        return res.status(500).send({error: error.message || "An unexpected server error occurred."});
     }
-    //res.send('added');
 });
 
 module.exports = router;
