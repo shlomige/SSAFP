0a7c8d15ec74c11b47727b46c6356e07416263d1
shlomige <shlomi@geva4u.com>
2025-06-09 22:00:42 +0300

    remove unnecessary routes

--- routes/contactus.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-    res.send('Contact us at : bla@bla.com ');
-});
-
-module.exports = router;
